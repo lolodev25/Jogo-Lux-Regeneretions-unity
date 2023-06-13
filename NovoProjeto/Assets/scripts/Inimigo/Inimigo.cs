6a03e7620c735f1805a8b355b4db8ee8889fa3b0
Cristiano Soares <cristianosoares.am@gmail.com>
2023-06-12 22:23:41 -0400

    atualizacao inimigo

--- NovoProjeto/Assets/scripts/Inimigo/Inimigo.cs
+++ NovoProjeto/Assets/scripts/Inimigo/Inimigo.cs
@@ -13,20 +13,46 @@ public class Inimigo : MonoBehaviour
     [SerializeField]
     private Rigidbody2D risgbory;
 
+    [SerializeField]
+    private SpriteRenderer spritrender;
+
+    [SerializeField]
+    private float distaciamin;
 
     // Update is called once per frame
     void Update()
     {
         Vector2 posicaoalvo = this.alvo.position;// pega a pisicao do jogador
         Vector2 posicaoatual = this.transform.position;
+
+        float distancia = Vector2.Distance(posicaoatual, posicaoalvo);
+        if(distancia>= this.distaciamin)
+        {
+
+        
+
         Vector2 direcao = posicaoalvo - posicaoatual;//calcula a distancia
         direcao = direcao.normalized;// deixa os valores entre -1 e 1
 
         this.risgbory.velocity = (this.velocidademov * direcao);// mover coforme o inimigo
-        
 
-        
 
+        if (this.risgbory.velocity.x > 0)//direita, inserir aq a animacao dele para a direita
+        {
+            this.spritrender.flipX = false;//olhando para a direita
+
+        }else if (this.risgbory.velocity.x < 0)//esquerda
+        {
+            this.spritrender.flipX = true;
+        }
+
+        }
+        else
+        {
+            //parar a movimentacao
+            this.risgbory.velocity = Vector2.zero;// zero no eixo x e y
+
+        }
 
     }
 }
